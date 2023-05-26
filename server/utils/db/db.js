7da166eac65543fe19848722ef407dfe24152b5e
Alon2303 <alonof27@gmail.com>
2023-05-26 16:25:10 +0300

    Parse volunteers file and update the db

---
+++ server/utils/db/db.js
@@ -0,0 +1,84 @@
+import pkg from 'pg';
+import { v4 as uuidv4 } from 'uuid';
+
+const { Client } = pkg;
+
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'postgres',
+  port: 5432,
+});
+
+const SCHEMA_NAME = "vat"
+const TABLE_NAME = "vat";
+
+export const connect = async () => {
+    console.log('connecting...');
+    await client.connect();
+    console.log('connected');
+}
+
+export const createSchema = async () => {
+    try {
+        await client.query(`CREATE SCHEMA IF NOT EXISTS ${SCHEMA_NAME}`)
+    } catch (error) {
+        console.log(`Failed to create schema: ${error}`);
+        client.end();
+    }
+};
+
+export const createTable = async () => {
+    try {
+        await client.query(
+            `CREATE TABLE IF NOT EXISTS ${SCHEMA_NAME}.${TABLE_NAME} (
+            v_uid varchar(255) primary key, \
+            heb_full_name varchar(255) not null, \
+            eng_full_name varchar(255) not null, \
+            email varchar(255) unique not null, \
+            gender varchar(255), \
+            role varchar(255), \
+            team varchar(255), \
+            faculty varchar(255), \
+            department varchar(255), \
+            year varchar(255),\
+            since int,\
+            phone varchar(255))`
+        );
+    } catch (error) {
+        console.log(error);
+        await client.end();
+    }
+}
+
+export const populatedata = async (volunteersData) => {
+    try {
+        const volunteersPromise = [];
+        for(const volunteer of volunteersData) {
+            console.log(volunteer["שם מלא באנגלית"]);
+            volunteersPromise.push(client.query(`INSERT INTO ${SCHEMA_NAME}.${TABLE_NAME}
+            (v_uid, heb_full_name, eng_full_name, email, gender, "role", team, faculty, department, "year", since, phone)
+            VALUES(
+                '${uuidv4()}',
+                '${volunteer["שם מלא בעברית"]}',
+                '${volunteer["שם מלא באנגלית"]}',
+                '${volunteer["כתובת מייל"]}',
+                '${volunteer["מין"]}',
+                '${volunteer["תפקיד בעמותה"]}',
+                '${volunteer["צוות"]}',
+                '${volunteer["פקולטה"]}',
+                '${volunteer["מחלקה"]}',
+                '${volunteer["שנה בתואר"]}',
+                '${volunteer["תאריך הצטרפות לארגון"]}',
+                '${volunteer["מספר טלפון"]}')`)
+            );
+        }
+        const res = await Promise.allSettled(volunteersPromise);
+        await client.end();
+        console.log(JSON.stringify(res, null, 2));
+    } catch (error) {
+        console.log(error);
+        await client.end();
+    }
+}
