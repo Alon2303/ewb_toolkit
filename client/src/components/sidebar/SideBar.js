6899cc26ddd0e5dbf76c6b68963b1ef632c4d05c
Yuval <yu.segal@gmail.com>
2023-08-13 15:35:45 +0300

    Add Theme Support across the app

--- client/src/components/sidebar/SideBar.js
+++ client/src/components/sidebar/SideBar.js
@@ -12,80 +12,86 @@ import EmailIcon from '@mui/icons-material/Email';
 import ChatIcon from '@mui/icons-material/Chat';
 import VideoCallIcon from '@mui/icons-material/VideoCall';
 import ScienceIcon from '@mui/icons-material/Science';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../../theme';
+import { Typography } from '@mui/material';
+
 import {
   useNavigate,
-} from "react-router-dom";
+} from 'react-router-dom';
 
 export default function BasicList() {
   let navigate = useNavigate();
 
   return (
-    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: '#1976d' }}>
-      <nav aria-label="options">
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton
-              onClick={() => {
-                navigate("/volunteers");
-              }}
-            >
-              <ListItemIcon>
-                <PeopleIcon color="primary"/>
-              </ListItemIcon>
-              <ListItemText primary="Volunteers" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton
-              onClick={() => {
-                navigate("/folder");
-              }}
-            >
-              <ListItemIcon>
-                <DraftsIcon color="primary"/>
-              </ListItemIcon>
-              <ListItemText primary="Knowledge Base" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <EmailIcon color="primary"/>
-              </ListItemIcon>
-              <ListItemText primary="Email" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <ChatIcon color="primary"/>
-              </ListItemIcon>
-              <ListItemText primary="Chat" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </nav>
-      <Divider />
-      <nav aria-label="secondary mailbox folders">
-        <List>
-        <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <VideoCallIcon color="primary"/>
-              </ListItemIcon>
-              <ListItemText primary="Video Chat" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <ScienceIcon color="primary"/>
-              </ListItemIcon>
-              <ListItemText primary="Matlab" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </nav>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: theme.palette.sidebar.main }}>
+        <nav aria-label="options">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton
+                onClick={() => {
+                  navigate('/volunteers');
+                }}
+              >
+                <ListItemIcon>
+                  <PeopleIcon />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography variant="h6" sx={{ color: theme.palette.primary.contrastText }}>
+                    Volunteers
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton
+                onClick={() => {
+                  navigate('/folder');
+                }}
+              >
+                <ListItemIcon>
+                  <DraftsIcon />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography variant="h6" sx={{ color: theme.palette.primary.contrastText }}>
+                    Knowledge Base
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </nav>
+        <Divider />
+        <nav aria-label="secondary mailbox folders">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <VideoCallIcon />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography variant="h6" sx={{ color: theme.palette.primary.contrastText }}>
+                    Video Chat
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <ScienceIcon />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography variant="h6" sx={{ color: theme.palette.primary.contrastText }}>
+                    Matlab
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </nav>
+      </Box>
+    </ThemeProvider>
   );
 }
