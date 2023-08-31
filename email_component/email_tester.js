4c206fd1ea63919574f21b1b091ffee1c7639ece
Aizikovich <eranaizi1995@gmail.com>
2023-08-31 20:37:40 +0300

    del old files

--- email_component/email_tester.js
+++
@@ -1,9 +0,0 @@
-const { sendEmail } = require('./email_back');
-
-const myEmail = "eranaizi1995@gmail.com";
-const mySubject = "Hello from EWB Toolkit";
-const myText = "This is a test email from the Node.js file - EWB Toolkit.";
-const myHtml = "<h1>This is a test <b>email<b> from the Node.js file - EWB Toolkit.</h1>";
-// Usage example:
-
-sendEmail(myEmail, mySubject, myText, myHtml);
